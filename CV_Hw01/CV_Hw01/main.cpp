9a4de17ff3ba505fa0250f0a8ec5e8bd6c1f66e9
ric113 <ric1134991@gmail.com>
2017-03-24 11:55:44 +0800

    讀入Image和Light Source file

--- CV_Hw01/CV_Hw01/main.cpp
+++ CV_Hw01/CV_Hw01/main.cpp
@@ -3,15 +3,87 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv/cvaux.hpp>
 #include <fstream>
+#include <stdio.h>
+
+#define IMG_COUNT 6
+
 
 using namespace std;
 using namespace cv;
 
-#define BYTE unsigned char
+
+int loadLightSource(Mat &LightSources)
+{
+    ifstream lightSource("test/bunny/LightSource.txt");
+    string line ;
+    Mat LightSourceArray[IMG_COUNT];
+    int index;
+    float x,y,z;
+    
+    
+    while(getline(lightSource, line))
+    {
+        if(sscanf(line.c_str(),"pic%d: (%f,%f,%f)",&index,&x,&y,&z) != 4)
+            return -1;
+        
+        cout << index << " " << x << " " << y << " " << z << endl;
+        
+        LightSourceArray[index-1] = (Mat_<float>(1,3) << x , y , z);
+    }
+    
+    
+    /* Combine to  6 * 3 Matrix (U Matrix) */
+    for(int i =0; i < IMG_COUNT; i++)
+    {
+        LightSourceArray[i].row(0).copyTo(LightSources.row(i));
+    }
+    
+    return 1;
+}
+
+int loadImage(Mat *Images)
+{
+    string path ;
+    for(int i = 0 ; i < IMG_COUNT ; i ++)
+    {
+        path = "test/bunny/pic" + to_string(i+1) + ".bmp";
+        Images[i] = imread(path, IMREAD_GRAYSCALE);
+        if(Images[i].data == NULL)
+            return -1;
+    }
+    return 1;
+}
 
 int main(int argc, const char * argv[])
 {
     
+    Mat Images[IMG_COUNT];
+    Mat LightSources(Mat::zeros(IMG_COUNT ,3, CV_64FC(1)));
+    
+    if(loadImage(Images) == -1)
+        cout << "Load image failure !" << endl;
+    
+    if(loadLightSource(LightSources) == -1)
+        cout << "Load ligt sources failure !" << endl;
+    
+    /*  Target : x = (U^TU)^-1 * U^T * y for all pixels
+     *  U (IMG# * 3): Light source Matrix .
+     *  y (IMG# * 1): pixels intensity . => Y (IMG# * (pixel#))
+     *  x (3 * 1) : pixel's normal .    => X (3 * (pixel#))
+     */
+    
+    int imageWidth = Images[0].cols,imageHeight = Images[0].rows;
+    int pixelAmount = imageWidth * imageHeight;
+    
+    Mat intensity(Mat::zeros(IMG_COUNT, pixelAmount, CV_64FC(1)));
+    
+    
+    
+    
+    
+    /*
+     
+     
     Mat Image = imread("test/bunny/pic1.bmp", IMREAD_GRAYSCALE);
     
     Mat tempImage = Image.clone();
@@ -27,7 +99,12 @@ int main(int argc, const char * argv[])
     tempImage.copyTo(result(Rect(0, 0, tempImage.cols, tempImage.rows)));
     smallImage.copyTo(result(Rect(0, tempImage.rows, smallImage.cols, smallImage.rows)));
     
-    imshow("CV", result);
+    */
+    
+    imshow("CV_window", Images[5]);   /* Params : Windows name / Mat */
+    
     waitKey();
     
 }
+
+
